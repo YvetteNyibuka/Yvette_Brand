0a56a9335527ceaf1fcd6471cdeb1e23adcf7df9
YvetteNyibuka <izanyibukayvette@gmail.com>
2023-08-25 10:22:11 +0200

    updates

--- index.js
+++
@@ -1,7 +0,0 @@
-let a = 7;
-
-console.log(a); 
-a= toString(a);
-length = a.length;
-
-console.log(length);
---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+document.getElementById("downloadButton").addEventListener("click", function() {
+    // Replace "path/to/your/pdf.pdf" with the actual path to your PDF document
+    var pdfUrl = "../docs/CV.pdf";
+    // Open the PDF in a new tab or window
+    window.open(pdfUrl, "_blank");
+  });
+
+  
